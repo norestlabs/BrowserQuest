c7cc0514925799ec73fa115669ae44676a3c700f
sunrisepopov <superfastdev16@gmail.com>
2019-01-15 05:34:19 +1000

    quick fix for breaking

--- src/engine/scripts/systems/FollowSystem.ts
+++ src/engine/scripts/systems/FollowSystem.ts
@@ -1,60 +1,62 @@
-import { System, registerSystem, SystemOrder} from "@engine/System";
+import { System, registerSystem, SystemOrder } from "@engine/System";
 import Entity from "@engine/Entity";
 import EntityManager from "@engine/EntityManager";
-import {BroadcastEvent} from "@engine/ecs";
-import {Follow, Movable, Transform, Identifiable} from "@components/Components";
-import { GameEvents, isEvent} from "@lib/GameEvents";
+import { BroadcastEvent } from "@engine/ecs";
+import { Follow, Movable, Transform, Identifiable } from "@components/Components";
+import { GameEvents, isEvent } from "@lib/GameEvents";
 import { ComponentNode, ComponentType, NodeType, createBitfield } from "@engine/Component";
 import { DestinationType } from "@components/Movable";
 
 interface FollowNode extends ComponentNode {
-    Follow : ComponentType<Follow>,
-    Transform : ComponentType<Transform>
+    Follow: ComponentType<Follow>,
+    Transform: ComponentType<Transform>
 }
 
 export default class FollowSystem implements System {
 
     s_name = "FollowSystem";
     enabled = true;
-    
-    public update () : void {
+
+    public update(): void {
         EntityManager.forEachEntityWithComponentNode(Follow, this.updateEntity, Transform);
     }
 
-    private updateEntity = (entity : Entity, node : NodeType<FollowNode>) : void => {
+    private updateEntity = (entity: Entity, node: NodeType<FollowNode>): void => {
         if (node.Follow.enabled && node.Follow.hasTarget()) {
             let movable = entity.getComponent(Movable);
             // Check movable's path
             let target = EntityManager.getEntityWithID(node.Follow.getTarget());
-            let targetTransform = target.getComponent(Transform);
-            /*if (movable === null || movable.wait) {
-                node.Transform.lookAt(targetTransform.Position);
-            }
-            else if (movable.isMoving()) {
+            if (target) {
+                let targetTransform = target.getComponent(Transform);
+                /*if (movable === null || movable.wait) {
+                    node.Transform.lookAt(targetTransform.Position);
+                }
+                else if (movable.isMoving()) {
+                    // Check if target has moved
+                    if (!targetTransform.GridPosition.equals(node.Follow.getLastKnowGridPosition())) {
+                        node.Follow.setLastKnowGridPosition(targetTransform.GridPosition);
+                        movable.setDestination(targetTransform.GridPosition.clone());
+                    }
+                }
+                else if (!targetTransform.GridPosition.isAdjacentNonDiagonal(node.Transform.GridPosition)) {
+                    movable.destination = targetTransform.GridPosition.clone();
+                }*/
+
+                // Check if it's moving
+                if (!movable.isMoving()) {
+                    // Start
+                    movable.setDestination(targetTransform.GridPosition.clone(), DestinationType.Around);
+                }
                 // Check if target has moved
                 if (!targetTransform.GridPosition.equals(node.Follow.getLastKnowGridPosition())) {
                     node.Follow.setLastKnowGridPosition(targetTransform.GridPosition);
-                    movable.setDestination(targetTransform.GridPosition.clone());
+                    movable.setDestination(targetTransform.GridPosition.clone(), DestinationType.Around);
                 }
-            }
-            else if (!targetTransform.GridPosition.isAdjacentNonDiagonal(node.Transform.GridPosition)) {
-                movable.destination = targetTransform.GridPosition.clone();
-            }*/
-
-            // Check if it's moving
-            if (!movable.isMoving()) {
-                // Start
-                movable.setDestination(targetTransform.GridPosition.clone(), DestinationType.Around);
-            }
-            // Check if target has moved
-            if (!targetTransform.GridPosition.equals(node.Follow.getLastKnowGridPosition())) {
-                node.Follow.setLastKnowGridPosition(targetTransform.GridPosition);
-                movable.setDestination(targetTransform.GridPosition.clone(), DestinationType.Around);
             }
         }
     }
 
-    public onNotify (params : any) : void {
+    public onNotify(params: any): void {
         if (isEvent(params, GameEvents.MouseClick)) {
             let entities = params.entities;
             let player = EntityManager.getEntityWithTag("Player");
@@ -76,11 +78,13 @@ export default class FollowSystem implements System {
                 let transform = params.origin.getComponent(Transform);
                 if (follow.hasTarget()) {
                     let target = EntityManager.getEntityWithID(follow.getTarget());
-                    if (transform.isAdjacentNonDiagonal(target.getComponent(Transform))) {
-                        BroadcastEvent(GameEvents.Follow_ReachedTarget.params(params.origin, target));
+                    if (target) {
+                        if (transform.isAdjacentNonDiagonal(target.getComponent(Transform))) {
+                            BroadcastEvent(GameEvents.Follow_ReachedTarget.params(params.origin, target));
+                        }
+                        // If stopped pathing without reaching target, stop follow
+                        follow.removeTarget();
                     }
-                    // If stopped pathing without reaching target, stop follow
-                    follow.removeTarget();
                 }
             }
         }
